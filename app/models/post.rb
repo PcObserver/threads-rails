79a5192813a5bea9d346a16b20898579db9d9f70
dgs-small <lucas9667@hotmail.com>
2023-11-29 13:15:29 -0300

    Implemented search bar to filter by post's title

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   has_many :comments, dependent: :destroy
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[d title body created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[comments]
+  end
 end
